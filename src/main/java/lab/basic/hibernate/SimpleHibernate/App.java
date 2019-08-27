48367abc4c6872ec871380b0261d24303d015f4f
imHarish03 <harishhari302@gmail.com>
2019-08-27 23:08:59 +0530

    * Simple hibernate create application

--- src/main/java/lab/basic/hibernate/SimpleHibernate/App.java
+++ src/main/java/lab/basic/hibernate/SimpleHibernate/App.java
@@ -1,13 +1,33 @@
 package lab.basic.hibernate.SimpleHibernate;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class App {
+	public static void main(String[] args) {
+		try {
+			//// Load Configuration
+			Configuration cfg = new Configuration();
+			cfg.addAnnotatedClass(lab.basic.hibernate.SimpleHibernate.User.class);
+			cfg.configure();
+			SessionFactory sessionFactory = cfg.buildSessionFactory();
+			Session session = sessionFactory.openSession();
+
+			Transaction transaction = session.beginTransaction();
+			User u = new User();
+			u.setName("Haris");
+
+			session.save(u);
+			transaction.commit();
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
 }
---
+++ src/main/java/lab/basic/hibernate/SimpleHibernate/User.java
@@ -0,0 +1,45 @@
+package lab.basic.hibernate.SimpleHibernate;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "person")
+public class User {
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "country")
+	private String country;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+}
