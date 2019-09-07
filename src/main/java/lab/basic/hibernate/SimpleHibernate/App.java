5b56c263504903aedd5b73b34cad3a2770d507ea
imHarish03 <harishhari302@gmail.com>
2019-09-07 22:34:26 +0530

    * Fetch data from DB by using get & load

--- src/main/java/lab/basic/hibernate/SimpleHibernate/App.java
+++ src/main/java/lab/basic/hibernate/SimpleHibernate/App.java
@@ -19,13 +19,41 @@ public class App {
 			SessionFactory sessionFactory = cfg.buildSessionFactory();
 			Session session = sessionFactory.openSession();
 
+			///// Save Data into Database
 			Transaction transaction = session.beginTransaction();
 			User u = new User();
-			u.setName("Haris");
+			u.setName("Saurabh");
 
 			session.save(u);
 			transaction.commit();
 
+			/// LoadExample
+			load(sessionFactory);
+			// getExample
+			get(sessionFactory);
+
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void load(SessionFactory sessionFactory) {
+		//////////////
+		try {
+			Session session2 = sessionFactory.openSession();
+			User getUser = (User) session2.load(User.class, 101);
+			System.out.println(getUser.getName());
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+	}
+
+	private static void get(SessionFactory sessionFactory) {
+		//////////////
+		try {
+			Session session2 = sessionFactory.openSession();
+			User getUser = (User) session2.get(User.class, 101);
+			System.out.println(getUser.getName());
 		} catch (Exception e) {
 			System.out.println(e);
 		}
